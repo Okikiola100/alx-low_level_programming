49d2e5431343d1bdb4b46b6bec429253038ddc1b
Oladapo <Oladapo>
2022-09-09 11:13:14 -0700

    Starting c language

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,21 +1,13 @@
 #include <stdio.h>
 
 /**
- *  * main - Entry point
- *   *
- *    * Author:Okikiola100
- *
- *     *
- *      * Return: Always 0 (Success)
- *       */
+ *  *  *main - Entry point
+ *   *   *Return: Always 0 (Success)
+ *    **/
+
 int main(void)
+
 {
-		char string[70] = "\"Programming is like building a multilingual puzzle\"";
-			
-			puts(string);
-
-				/* Tried printing it directly */
-
-				puts("\"Programming is like building a multilingual puzzle\"");
+		puts("\"Programming is like building a multilingual puzzle");
 					return (0);
 }
--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,8 @@
 #include <stdio.h>
-
 /**
  *  * main - Entry point
  *   *
- *    * Return: Always 0 (Success)
+ *    * Return: always 0
  *     */
 int main(void)
 {
