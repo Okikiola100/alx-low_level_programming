08864b6e8d5c90ab6e6dddae6bf89a1579222165
Oladapo <Oladapo>
2022-09-10 04:10:25 -0700

    Done with C hello world

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,7 +3,7 @@
 /**
  *  * main - prints exactly "and that piece of art is useful"
  *   * Return: Always 0 (Success)
- *    */
+ */
 int main(void)
 {
 	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
