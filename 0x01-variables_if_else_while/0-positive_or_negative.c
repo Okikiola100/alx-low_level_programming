014ac2f28729d4174e9c090adf53ddd0da0f1a4b
Oladapo <Oladapo>
2022-09-10 14:18:38 -0700

    My betty ooo

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -4,24 +4,15 @@
 /**
  *  *main - Entry point
  *   *Return: Always 0 (Success)
- *    */
-int main(void)
-{
-		int n;
-
-			srand(time(0));
-				n = rand() - RAND_MAX / 2;
-					if (n > 0)
-							{
-									printf("%i is positive\n", n);
-										}
-						else if (n == 0)
-								{
-										printf("%i is zero\n", n);
-											}
-							else if (n < 0)
-									{
-											printf("%i is negative\n", n);
-												}
-								return (0);
+ */
+int main(void){
+int n;
+srand(time(0));
+n = rand() - RAND_MAX / 2;		if (n > 0){
+	printf("%i is positive\n", n);
 }
+else if (n =0){				printf("%i is zero\n", n);	}
+else if (n < 0){
+	printf("%i is negative\n", n);					}
+return (0=);
+}}
