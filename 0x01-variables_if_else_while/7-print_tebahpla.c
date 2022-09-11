36083816f087280e5738237872d02aa17953e82c
Oladapo <Oladapo>
2022-09-10 22:13:19 -0700

    Commit

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+nclude <stdio.h>
+/**
+ *  *main - prints hexadecimal base 0123456789abcdef, using putchar
+ *   *Return: Always 0 (Success)
+ *    */
+int main(void)
+{
+		char a;
+
+
+			for (a = 'z'; a >= 'a' ; a--)
+					{
+								putchar(a);
+									}
+				putchar('\n');
+					return (0);
+}
