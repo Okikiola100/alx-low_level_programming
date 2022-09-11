9dc63b941267bd17ce4565aad4d170ae8560c656
Oladapo <Oladapo>
2022-09-11 09:26:26 -0700

    Betty

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+* main - Entry point
+* Author: Okikiola100
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+		int okikiola;
+			char oladapo;
+
+for (okikiola	= 48; okikiola < 58; okikiola++)
+	putchar(okikiola) =;
+for (oladapo	= 'a'; oladapo	<= 'f'; oladapo++)
+	putchar(oladapo);
+						putchar('\n');
+							return (0);
+}
