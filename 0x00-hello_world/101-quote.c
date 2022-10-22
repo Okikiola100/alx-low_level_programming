846e5266141b1f454e188e626fc731ab67c9bb1a
Oladapo <Oladapo>
2022-10-21 23:27:34 -0700

    C- Hello World!

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- *  * main - prints exactly "and that piece of art is useful"
- *   * Return: Always 0 (Success)
- */
+ * main - prints exactly "and that piece of art is useful"
+ * Return: Always 0 (Success)
+*/
 int main(void)
 {
 	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
--- 0x00-hello_world/README.md
+++ 0x00-hello_world/README.md
@@ -1,1 +1,6 @@
-Introduction to C program language
+# Introduction to C program language
+## C Hello World!
+Author: Oladapo Okikiola a.k.a Oladapo Okikiola
+### Read my blog:
+Okikiola100.hasnode.dev
+#### Thanks for following
